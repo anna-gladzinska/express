20e65c52188b0bcbca04bd7f6395f2bfb8bbfd5e
Anna Gladzińska <agladzinska@gmail.com>
2020-04-25 16:59:21 +0200

    Add express

---
+++ shopserver.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/admin', (req, res, next) => {
+    if (isAdmin()) next();
+    else res.send('Go away!');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/cart.html'));
+});
+
+app.get('/admin/products', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/admin/products.html'));
+});
+
+app.get('/admin/payments', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/admin/payments.html'));
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
